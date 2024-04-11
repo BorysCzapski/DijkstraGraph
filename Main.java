cd93577349d973ff6a7b05fbd83d5c7289c91707
BorysCzapski <113203258+BorysCzapski@users.noreply.github.com>
2024-04-11 09:32:50 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,23 @@
+public class Main {
 
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+
+        graph.addEdge(5, 1, 2);
+        graph.addEdge(4, 1, 3);
+        graph.addEdge(2, 2, 4);
+        graph.addEdge(2, 3, 4);
+        graph.addEdge(3, 1, 4);
+        graph.addEdge(7, 2, 3);
+
+        graph.dijkstra(1);
+
+
+        graph.printGraph();
+    }
+
+}
